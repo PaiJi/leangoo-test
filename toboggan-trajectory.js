d28c7106f74e9ed6875932edfaa9a4e8adf91cd4
PaiJi <github@jipai.moe>
2021-11-12 14:09:41 +0800

    format code

--- toboggan-trajectory.js
+++ toboggan-trajectory.js
@@ -1,25 +1,24 @@
+function findTrees(treeArea) {
+  const splitTreeArea = treeArea.split("\n");
+  const lineLength = treeArea.indexOf("\n");
+  let x = 0,
+    trees = 0;
 
-function findTrees(treeArea){
-    const splitTreeArea=treeArea.split('\n')
-    const lineLength=treeArea.indexOf('\n')
-    let x=0,trees=0
-
-    splitTreeArea.forEach((line,lineNum)=>{
-        const treeLineArray=line.split('')
-        if(treeLineArray[x]==='#'){
-            trees++
-        }
-        if(x+3>=lineLength){
-            x=x+3-lineLength
-        }else{
-            x=x+3
-        }
-    })
-    return trees
+  splitTreeArea.forEach((line, lineNum) => {
+    const treeLineArray = line.split("");
+    if (treeLineArray[x] === "#") {
+      trees++;
+    }
+    if (x + 3 >= lineLength) {
+      x = x + 3 - lineLength;
+    } else {
+      x = x + 3;
+    }
+  });
+  return trees;
 }
 
-
-const text=`....#..#.......#........#....#.
+const text = `....#..#.......#........#....#.
 ..##.#.#.#...................#.
 ....#.#.##..#....#......#......
 .........#.#......##.....#.....
@@ -341,6 +340,6 @@ const text=`....#..#.......#........#....#.
 ..#...........##...........#...
 ..............#.........#.....#
 ..#....#..#...#...##.#.........
-.#.#.#....#..........#.........`
+.#.#.#....#..........#.........`;
 
-console.log(findTrees(text))
+console.log(findTrees(text));
